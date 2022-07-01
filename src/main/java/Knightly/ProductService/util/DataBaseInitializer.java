a0dfb7261daff584832f6998794d9c2d6c67372a
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2022-07-01 12:58:21 +0200

    redis cachin for DTO service. little bit of cleanup from earlier troubleshooting

--- src/main/java/Knightly/ProductService/util/DataBaseInitializer.java
+++ src/main/java/Knightly/ProductService/util/DataBaseInitializer.java
@@ -1,13 +1,20 @@
 package Knightly.ProductService.util;
 
+import Knightly.ProductService.dto.ComponentDTO;
+import Knightly.ProductService.dto.ProductDTO;
+import Knightly.ProductService.enums.Currency;
 import Knightly.ProductService.model.Component;
 import Knightly.ProductService.model.Product;
 import Knightly.ProductService.repository.ComponentRepository;
 import Knightly.ProductService.repository.ProductRepository;
+import Knightly.ProductService.service.DTOService;
+import lombok.experimental.Accessors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -17,6 +24,8 @@ public class DataBaseInitializer {
     private final String WAREHOUSE_COMPONENTS_URL = "http://localhost:6868/components";
     private final WarehouseComponentsGetter warehouseComponentsGetter = new WarehouseComponentsGetter();
     private final WarehouseProductsGetter warehouseProductsGetter = new WarehouseProductsGetter();
+    @Autowired
+    DTOService dtoService;
 
     @Bean
     CommandLineRunner initializeDatabase(ComponentRepository componentRepository, ProductRepository productRepository) {
