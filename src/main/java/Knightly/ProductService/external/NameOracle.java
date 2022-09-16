851998faaff2014aa3e78199f76d48c5aac355ad
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2022-09-16 13:18:51 +0200

    Oracle returns response entity as extraction of name doesnt work

--- src/main/java/Knightly/ProductService/external/NameOracle.java
+++ src/main/java/Knightly/ProductService/external/NameOracle.java
@@ -2,8 +2,6 @@ package Knightly.ProductService.external;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -14,24 +12,15 @@ public class NameOracle {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String nameURI = "https://api.agify.io";
 
-    public String getAge(@RequestParam("name") String name) {
+    public ResponseEntity<String> getAge(String name) {
         String requestURI = this.nameURI + "/?name=" + name;
         HttpHeaders header = new HttpHeaders();
         header.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
         HttpEntity<String> entity = new HttpEntity<>("parameter", header);
 
-        ResponseEntity<String> responseEntity = restTemplate.exchange(requestURI, HttpMethod.GET, entity, String.class);
-        return getNameFromResponseEntitiy(responseEntity);
+        return restTemplate.exchange(requestURI, HttpMethod.GET, entity, String.class);
+
     }
 
-    private String getNameFromResponseEntitiy(ResponseEntity<String> responseEntity) {
-        try {
-            Map<String, Object> responseMap = new com.fasterxml.jackson.databind.ObjectMapper().readValue("Name", Map.class);
-
-            return responseMap.get("fileContent").toString();
-        } catch (JsonProcessingException e) {
-            return "Error reading the name from Response";
-        }
-    }
 }
