e7d089235e9776049603b72fdb166429a425f451
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2022-09-17 09:26:33 +0200

    docker-compose working somewhat

--- src/main/java/Knightly/ProductService/util/DataBaseInitializer.java
+++ src/main/java/Knightly/ProductService/util/DataBaseInitializer.java
@@ -1,27 +1,24 @@
 package Knightly.ProductService.util;
 
-import Knightly.ProductService.api.dto.ComponentDTO;
-import Knightly.ProductService.api.dto.ProductDTO;
-import Knightly.ProductService.enums.Currency;
 import Knightly.ProductService.repository.jpa.Component;
 import Knightly.ProductService.repository.jpa.Product;
 import Knightly.ProductService.repository.ComponentRepository;
 import Knightly.ProductService.repository.ProductRepository;
-import Knightly.ProductService.service.DTOService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataIntegrityViolationException;
 
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
 public class DataBaseInitializer {
 
-    private final String WAREHOUSE_PRODUCTS_URL = "http://localhost:6868/products";
-    private final String WAREHOUSE_COMPONENTS_URL = "http://localhost:6868/components";
+    private final String WAREHOUSE_PRODUCTS_URL = "http://WarehouseApp:8080/products";
+    private final String WAREHOUSE_COMPONENTS_URL = "http://WarehouseApp:8080/components";
     private final WarehouseComponentsGetter warehouseComponentsGetter = new WarehouseComponentsGetter();
     private final WarehouseProductsGetter warehouseProductsGetter = new WarehouseProductsGetter();
     private static final Logger logger = LoggerFactory.getLogger(DataBaseInitializer.class);
@@ -35,7 +32,7 @@ public class DataBaseInitializer {
 
                 List<Product> products = warehouseProductsGetter.getProductsFromWarehouse(WAREHOUSE_PRODUCTS_URL);
                 productRepository.saveAll(products);
-            } catch (Exception e) {
+            } catch (DataIntegrityViolationException e) {
                logger.error("Data has already been fetched");
             }
         };
