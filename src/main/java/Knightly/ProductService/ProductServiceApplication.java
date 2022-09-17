daff14a1fdc373c5fcc3d5d2501797a2d9b824f2
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2022-09-17 14:15:55 +0200

    added dependencies for fix

--- src/main/java/Knightly/ProductService/ProductServiceApplication.java
+++ src/main/java/Knightly/ProductService/ProductServiceApplication.java
@@ -5,12 +5,10 @@ package Knightly.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
-
+import org.springframework.retry.annotation.EnableRetry;
 
 
 @SpringBootApplication
-@EnableCaching
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
