12d9ebdf33775454864778a712709196845c68e9
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2022-09-18 12:56:24 +0200

    first draft for Request objects

---
+++ src/main/java/Knightly/ProductService/enums/RequestType.java
@@ -0,0 +1,11 @@
+package Knightly.ProductService.enums;
+
+public enum RequestType {
+    getComponents,
+    getProducts,
+    getUser,
+    createProduct,
+    emptyShoppingCart,
+    updateShoppingCart,
+    getAge
+}
---
+++ src/main/java/Knightly/ProductService/server/dto/ProductRequest.java
@@ -0,0 +1,47 @@
+package Knightly.ProductService.server.dto;
+
+import Knightly.ProductService.enums.Currency;
+import Knightly.ProductService.enums.RequestType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProductRequest {
+
+        public RequestType requestType;
+        public Currency currency;
+        public List<Long> componentIDs;
+        public Long userID;
+        public String productName;
+        public String oracleName;
+
+        public ProductRequest(RequestType requestType) {
+                this.requestType = requestType;
+                this.currency = Currency.bronze;
+        }
+
+        public ProductRequest(RequestType requestType, Currency currency) {
+                this(requestType);
+                this.currency = currency;
+        }
+         public ProductRequest(RequestType requestType, List<Long> componentIDs, String productName){
+                this(requestType);
+                this.componentIDs = componentIDs;
+                this.productName = productName;
+         }
+
+         public ProductRequest(RequestType requestType, Long userID){
+                this(requestType);
+                this.userID = userID;
+         }
+
+         public ProductRequest(RequestType requestType, String oracleName){
+            this(requestType);
+            this.oracleName = oracleName;
+         }
+}
