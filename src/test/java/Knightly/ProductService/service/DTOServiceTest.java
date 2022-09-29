b208969da929510078c994638af6c02d6c877215
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2022-09-29 09:38:13 +0200

    more tests

--- src/test/java/Knightly/ProductService/service/DTOServiceTest.java
+++ src/test/java/Knightly/ProductService/service/DTOServiceTest.java
@@ -1,9 +1,9 @@
 package Knightly.ProductService.service;
 
-import Knightly.ProductService.enums.Currency;
 import Knightly.ProductService.microservices.CurrencyConversionGetter;
 import Knightly.ProductService.microservices.PriceGetter;
 import Knightly.ProductService.repository.jpa.Component;
+import Knightly.ProductService.repository.jpa.Product;
 import Knightly.ProductService.service.impl.DTOServiceImpl;
 import Knightly.ProductService.service.impl.DataServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -30,18 +30,63 @@ public class DTOServiceTest {
     private CurrencyConversionGetter currencyConversionGetter;
 
     @Test
-    public void getComponents() {
-        Mockito.when(dataService.getComponents()).thenReturn(getComponentList());
+    public void getAllComponentDTOs() {
         dtoService.getAllComponentDTOs();
 
         Mockito.verify(dataService, Mockito.times(1)).getComponents();
     }
 
     @Test
-    public void getProducts() {
+    public void getAllProductDTOs() {
         dtoService.getAllProductDTOs();
 
         Mockito.verify(dataService, Mockito.times(1)).getProducts();
+    }
+
+
+    @Test
+    public void createProduct() {
+        Product product = getProduct();
+
+        dtoService.createProduct(product);
+
+        Mockito.verify(dataService, Mockito.times(1)).createProduct(product);
+
+    }
+
+    @Test
+    public void createProductFromIDs() {
+        dtoService.createProductFromIDs(List.of(2L,4L,10L), "Obama");
+
+        Mockito.verify(dataService, Mockito.times(1)).createProduct(any());
+    }
+
+    private Product getProduct() {
+        return new Product(20L, "bleh",
+                List.of(new Component(
+                        8L
+                        , "boy"
+                        , 1
+                        , "next"
+                        , 3
+                        , 4
+                        , "door"
+                        , 3
+                        , 3
+                        , 4
+                ), new Component(
+                15L
+                , "some"
+                , 1
+                , "body"
+                , 3
+                , 4
+                , "once"
+                , 3
+                , 3
+                , 4
+        )));
+
     }
 
     private List<Component> getComponentList() {
